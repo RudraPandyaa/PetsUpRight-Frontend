b7c79c9af40f1dd373cf18e1e966c9192add1c23
sumitj31 <sumitj31012005@gmail.com>
2026-08-26 16:46:59 +0530

    added cart section when click on cart, Fixed some UI issues in homepage,fixed one image issue in shop page and done some UI changes

--- app/components/layout/AppHeader.vue
+++ app/components/layout/AppHeader.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import CartDrawer from '~/components/cart layout/cartlayout.vue'
+
 const isMobileMenuOpen = ref(false)
 const isDogsOpen = ref(false)
 const isCatsOpen = ref(false)
+
+const isCartOpen = ref(false)
+
 const cartCount = ref(2)
 const wishlistCount = ref(0)
 const searchQuery = ref('')
@@ -134,17 +140,34 @@ const searchQuery = ref('')
               </span>
             </NuxtLink>
 
-            <NuxtLink to="/cart" class="relative p-1 text-[#1a1a2e] hover:text-[#44476f] transition" aria-label="Cart">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-              </svg>
-              <span
-                v-if="cartCount > 0"
-                class="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#44476f] text-[10px] font-bold text-white"
-              >
-                {{ cartCount }}
-              </span>
-            </NuxtLink>
+            <button
+  type="button"
+  class="relative p-1 text-[#1a1a2e] hover:text-[#44476f] transition"
+  aria-label="Cart"
+  @click.stop="isCartOpen = true"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707-1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+    />
+  </svg>
+
+  <span
+    v-if="cartCount > 0"
+    class="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#44476f] text-[10px] font-bold text-white"
+  >
+    {{ cartCount }}
+  </span>
+</button>
 
             <NuxtLink to="/store-locator" class="hidden sm:block p-1 text-[#1a1a2e] hover:text-[#44476f] transition" aria-label="Store locator">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -205,4 +228,8 @@ const searchQuery = ref('')
       </div>
     </div>
   </header>
+  <CartDrawer
+    :is-open="isCartOpen"
+    @close="isCartOpen = false"
+  />
 </template>
